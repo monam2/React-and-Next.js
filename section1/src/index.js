53cb19ba5a53cbf959c28a611627a9c576154316
monam2 <kangcw0107@gmail.com>
2023-11-03 01:21:19 +0900

    node js module system

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello Node.js");
+// const moduleDate = require("./math");
+// const { add, sub } = require("./math");
+// 위의 두개는 CJS모듈 시스템
+
+import  mul, {add, sub} from "./math.js"
+
+// import mul from "./math.js"
+//ESM은 import {..} form  "경로/파일명.js"
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(10, 20));
+
+// console.log(moduleDate.add(1,2));
+// console.log(moduleDate.sub(1,2));
---
+++ section1/src/math.js
@@ -0,0 +1,20 @@
+export function add(a,b) {
+    return a+b;
+}
+
+export function sub(a,b) {
+    return a-b;
+}
+
+export default function multiply(a,b) {
+    return a*b;
+}
+
+// export {add,sub};
+//ESM
+
+//CJS 모듈시스템
+// module.exports = { 
+//     add,
+//     sub,
+// };
