d152d6593b994c01877a367794fa01b3cd378096
monam2 <kangcw0107@gmail.com>
2023-11-05 19:26:16 +0900

    props

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -1,10 +1,27 @@
-import './Body.css'
+import './Body.css';
+import Button from './Button';
+
+function ButtonChild() {
+    return <div>ButtonChild</div>
+}
 
 export default function Body() {
+    const buttonProps = {
+        text : "1번 버튼",
+        color : "red",
+        a : 1,
+        b : 2,
+        c : 3,
+    }
 
     return (
         <div className="body">
             <h1>body</h1>
+            <Button {...buttonProps}>
+                <ButtonChild />
+            </Button>
+            <Button text={"2번 버튼"} />
+            <Button text={"3번 버튼"} />
         </div>
     )
 }
