03987507d9933ac047b1f51b70d161836a19d7b4
monam2 <kangcw0107@gmail.com>
2023-11-08 00:04:08 +0900

    11.08

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -1,45 +1,57 @@
 import './Body.css';
 import { useState } from 'react';
 
-function LightBulb({light}) {
-    return(
-        <>
-        {light === 'ON' ?
-        <div style={{backgroundColor:"orange"}}>{light}</div> :
-        <div style={{backgroundColor:"gray"}}>{light}</div>
-        }</>
-    )
-}
-
-function StaticLightBulb() {
-    console.log(1)
-    return (
-        <div style={{backgroundColor:"gray"}}>OFF</div>
-        
-    )
-}
-
 export default function Body() {
-    const [light, setLight] = useState("OFF");
+const [state, setState] = useState({
+    name : "",
+    gender : "",
+    bio : ""
+});
+
+    console.log(state.name)
+    console.log(state.gender)
+    console.log(state.bio)
+
+    const onChange = (e)=>{
+        setState({
+            ...state,
+            [e.target.name] : e.target.value,
+    })
+    }
+
+    // const onChangeName = (e) => {
+    //     setState({
+    //         ...state, name : e.target.value,
+    //     })
+    // }
+
+    // const onChangeGender = (e)=>{
+    //     setState({
+    //         ...state, gender : e.target.value,
+    //     })
+    // }
+
+    // const onChangeBio = (e)=>{
+    //     setState({
+    //         ...state, bio : e.target.value,
+    //     })
+    // }
 
     return (
         <div className="body">
-        <LightBulb light={light}/>
-        <StaticLightBulb />
-        <button onClick={()=>{
-            setLight("OFF");
-            }
-        }>끄기</button>
-        <button onClick={()=>{
-            setLight("ON");
-            }
-        }>켜기</button>
-
-
-        {/* <button onClick={()=>{
-            setLight("OFF")
-        }}>끄기</button> */}
-        
+            <div>
+                <input name = {"name"} value={state.name} onChange={onChange} />
+            </div>
+            <div>
+                <select name = {"gender"} value = {state.gender} onChange={onChange}>
+                    <option value="">밝히지 않음</option>
+                    <option value="female">여성</option>
+                    <option value="male">남성</option>
+                </select>
+            </div>
+            <div>
+                <textarea name = {"bio"} value = {state.bio} onChange={onChange}/>
+            </div>
         </div>
     )
 }
