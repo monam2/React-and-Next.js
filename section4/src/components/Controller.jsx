9656b8a90f7f9ab684c2530280113ab846574f8c
monam2 <kangcw0107@gmail.com>
2023-11-08 14:49:07 +0900

    11.08 counter ui

---
+++ section4/src/components/Controller.jsx
@@ -0,0 +1,15 @@
+import './Controller.css'
+
+export default function Controller() {
+
+    return (
+        <div>
+            <button>-1</button>
+            <button>-10</button>
+            <button>-100</button>
+            <button>+100</button>
+            <button>+10</button>
+            <button>+1</button>
+        </div>
+    )
+}
