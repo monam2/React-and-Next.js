6e10d27f5c6e91961a79c1ef4d0938ae2867996c
monam2 <kangcw0107@gmail.com>
2023-11-10 00:10:43 +0900

    11.10

--- section4/src/App.jsx
+++ section4/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
 import Controller from './components/Controller'
 import Viewer from './components/Viewer'
-import {useState} from 'react';
+import Even from './components/Even';
+import {useState, useEffect, useRef} from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const isMountRef = useRef(false);
+
+  useEffect(()=>{
+
+    if (!isMountRef.current) {
+      isMountRef.current = true;
+      return;
+    }
+    console.log("업데이트");
+  });
+
+  useEffect(()=>{
+    console.log("마운트");
+  },[]);
 
   const onClickButton = (value)=>{
     setCount(count + value);
@@ -12,13 +29,19 @@ function App() {
   const resetButton = ()=>{
     setCount(0);
   }
+  const onChangeText = (e)=>{
+    setText(e.target.value);
+  }
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <input value={text} onChange={onChangeText} />
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 && <Even />}
       </section>
+      
       <section>
         <Controller
         onClickButton={onClickButton}
