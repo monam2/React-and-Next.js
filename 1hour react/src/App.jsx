2384bcfd300c6676821eff08943669370107ae5c
monam2 <kangcw0107@gmail.com>
2023-11-23 13:39:26 +0900

    react

---
+++ 1hour react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+
+      <div className="App">asd</div>
+      <Home/>
+    </>
+  )
+}
+
+export default App
