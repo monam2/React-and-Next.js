4dab0137bbba3df7e50eb06e14ce64379f97c2d1
monam2 <kangcw0107@gmail.com>
2023-12-27 21:24:08 +0900

    todo update

--- section7/src/App.jsx
+++ section7/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useRef, useReducer } from 'react'
 import './App.css'
 import Header from './components/Header'
 import TodoEditor from './components/TodoEditor'
@@ -6,67 +6,87 @@ import TodoList from './components/TodoList'
 
 const mockData = [
   {
-    id : 0,
-    isDone : true,
-    content : "React 공부하기",
-    createdDate : new Date().getTime(),
+    id: 0,
+    isDone: true,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
   },
   {
-    id : 1,
-    isDone : false,
-    content : "빨래 널기",
-    createdDate : new Date().getTime(),
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
   },
   {
-    id : 2,
-    isDone : true,
-    content : "음악 연습하기",
-    createdDate : new Date().getTime(),
+    id: 2,
+    isDone: true,
+    content: "음악 연습하기",
+    createdDate: new Date().getTime(),
   },
 ];
 
-function App() {
+const reducer = (state, action) => {
 
-  const [todos, setTodos] = useState(mockData);
+  switch (action.type) {
+    case "CREATE": return [...state, action.data];
+    case "UPDATE": return state.map((it) => it.id === action.data ? { ...it, isDone: !it.isDone } : it);
+    case "DELETE": return state.filter((it) => it.id !== action.data);
+  };
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content)=>{
-    const newTodo = {
-      id : idRef.current++,
-      isDone : false,
-      content,
-      createdDate : new Date().getTime(),
-    }
-
-    setTodos([...todos, newTodo]);
-  }
-
-  const onUpdate = (targetId)=>{
-    setTodos(
-      todos.map(todo=>{
-        if(todo.id === targetId) { // isDone의 값을 바꿔주면 됨
-          return {...todo, isDone : !todo.isDone}
-        } else { // isDone의 값을 내버려두면 됨
-          return todo;
+  const onCreate = (content) => {
+    dispatch(
+      {
+        type: "CREATE",
+        data: {
+          id: idRef.current++,
+          isDone: false,
+          content,
+          createdDate: new Date().getTime(),
         }
       })
-    )
+    // setTodos([...todos, newTodo]);
+  }
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      data: targetId,
+    });
+
+    //   setTodos(
+    //     todos.map(todo=>{
+    //       if(todo.id === targetId) { // isDone의 값을 바꿔주면 됨
+    //         return {...todo, isDone : !todo.isDone}
+    //       } else { // isDone의 값을 내버려두면 됨
+    //         return todo;
+    //       }
+    //     })
+    //   )
   };
 
-  const onDelete = (targetId) =>{
-    setTodos(
-      todos.filter((todo)=>todo.id!==targetId)
-    )
+  const onDelete = (targetId) => {
+    // setTodos(
+    //   todos.filter((todo)=>todo.id!==targetId)
+    // )
+
+    dispatch({
+      type: "DELETE",
+      data: targetId,
+    })
 
   }
 
   return (
     <div className="App">
       <Header />
-      <TodoEditor onCreate={onCreate}/>
-      <TodoList todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
+      <TodoEditor onCreate={onCreate} />
+      <TodoList todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   )
 }
-
-export default App
+export default App;
--- section7/src/main.jsx
+++ section7/src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import A from './exam/A.jsx'
-import B from './exam/B.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <B/>
+    <App />
   </React.StrictMode>,
 )
